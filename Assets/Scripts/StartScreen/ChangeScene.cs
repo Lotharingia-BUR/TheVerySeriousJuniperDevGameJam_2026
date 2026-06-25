d920c6503d37b8f0b1d29c8267516e4d7ea7e5f1
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-06-25 02:11:44 -0400

    go to sleeep

--- Assets/Scripts/StartScreen/ChangeScene.cs
+++ Assets/Scripts/StartScreen/ChangeScene.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
@@ -6,17 +7,33 @@ public class ChangeScene : MonoBehaviour
     public float time;
     public Button sceneButton;
     public Animator sceneAnimator;
+    public Animator UIAnimator;
     public int sceneIndex;
+    private bool isSceneChange;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sceneButton.onClick.AddListener(SceneChange);
+        isSceneChange = false;
+    }
+
+    void SceneChange()
+    {
+        sceneAnimator.SetBool("isStart", true);
+        UIAnimator.SetBool("isStart", true);
+        isSceneChange = true;
+        time = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
+
+        if(time > 11.1f && isSceneChange)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
 
     }
 }
