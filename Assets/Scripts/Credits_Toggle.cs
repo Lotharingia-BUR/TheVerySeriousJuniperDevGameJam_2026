f183190b0fc1e37e222a8823495ea6364e3d2ab8
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-06-26 20:08:00 -0400

    cooler Animation

---
+++ Assets/Scripts/Credits_Toggle.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits_Toggle : MonoBehaviour
+{
+    public Button creditsButton;
+    public Image creditsPage;
+    private bool isOn;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        creditsButton.onClick.AddListener(toggleCredits);
+        isOn = false;
+    }
+
+    void toggleCredits()
+    {
+        if (!isOn)
+        {
+            creditsPage.GetComponent<Image>().color = Color.clear;
+            isOn = true;
+        }
+        else if (isOn)
+        {
+            creditsPage.GetComponent<Image>().color = Color.white;
+            isOn = false;
+        }
+    }
+}
